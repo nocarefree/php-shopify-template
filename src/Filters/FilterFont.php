655fb86c07ad9de1b25890b220aca55886c9d155
nocarefree <170890803@qq.com>
2022-07-23 11:27:35 +0800

    update FilterFont

--- src/Filters/FilterFont.php
+++ src/Filters/FilterFont.php
@@ -10,14 +10,26 @@ class FilterFont{
 
 
     public static function font_modify(FontDrop $input , $key, $value){
+        if(!($input instanceof FontDrop)){
+            return '';
+        }
+
         return $input->modify($key, $value);
     }
 
     public static function font_face(FontDrop $input , $key){
+        if(!($input instanceof FontDrop)){
+            return '';
+        }
+
         return $input->toHtml($key);
     }
 
     public static function font_url(FontDrop $input, $key = null){
+        if(!($input instanceof FontDrop)){
+            return '';
+        }
+        
         return $input->url($key);
     }
 
