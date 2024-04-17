ae3d877eb7365fa0cd7304759699fe38e5be0cf6
nocarefree <170890803@qq.com>
2024-04-17 23:50:29 +0800

    update

---
+++ src/Filters.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace ShopifyTemplate;
+
+use Illuminate\Support\Arr;
+
+// https://shopify.dev/api/liquid/filters/font-filters
+
+class Filters
+{
+    protected $locale;
+
+    public function __construct($config = [])
+    {
+        $this->locale = $config['locale'] ?? [];
+    }
+    public function t($input, $data = [])
+    {
+        $input = Arr::get($this->locale, $input, '');
+        foreach ($data as $key => $value) {
+            $input = preg_replace("/{{\s*" . preg_quote($key, '/') . "\s*}}/", $value, $input);
+        }
+        return $input;
+    }
+}
